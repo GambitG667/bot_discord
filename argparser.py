a5d6aa73d1677fdd1e2d5aa9eec118c35d1f3b9b
destime224 <destim224@gmail.com>
2024-03-31 18:20:12 +0300

    Добавлен ключ для создания вебхука

--- argparser.py
+++ argparser.py
@@ -7,5 +7,6 @@ argparser.add_argument("--out", action=argparse.BooleanOptionalAction, default=T
 argparser.add_argument("--out-file", action=argparse.BooleanOptionalAction, default=True, help="Логирование в файле (по умолчанию включено)")
 argparser.add_argument("--logger-level", "-l", choices=("DEBUG", "INFO", "WARNING", "ERROR", "CRITICAL"), default="INFO", help="Уровень логирования")
 argparser.add_argument("--token", "-t", metavar="TOKEN_NAME", type=str, default="TOKEN", help="Имя токена в окружении")
+argparser.add_argument("--webhook", "-w", metavar="WEBHOOK_ID", type=int, default=None, help="При указании ключа бот будет использовать данный вебхук для отправки ошибок в канал.")
 
 args = argparser.parse_args()
